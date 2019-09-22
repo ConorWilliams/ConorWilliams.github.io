26ab5274cb140b833730fa01705bab114f7a0402
ConorWilliams <conorwilliams@outlook.com>
2019-09-22 18:41:08 +0100

    index

--- generate.sh
+++ generate.sh
@@ -4,6 +4,6 @@ echo 'Village Reports' > index.html
 
 for filename in eWater/*.html; do
     [ -e "$filename" ] || continue
-    echo '<a href="eWater/'$filename'">'$filename'</a>' >> index.html
+    echo '<a href="'$filename'">'$filename'</a>' >> index.html
 
 done
