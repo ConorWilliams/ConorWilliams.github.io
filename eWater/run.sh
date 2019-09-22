50b84e453ca3fab9bf45f8eeb1d877a40149e4dd
ConorWilliams <conorwilliams@outlook.com>
2019-09-23 00:06:40 +0100

    all villages

--- eWater/run.sh
+++ eWater/run.sh
@@ -1,5 +1,5 @@
 for filename in *.csv; do
     [ -e "$filename" ] || continue
     echo 'Doing' $filename
-    taptimise $filename 10000 --scribble 200
+    taptimise $filename 10000 --scribble 200 -s200
 done
