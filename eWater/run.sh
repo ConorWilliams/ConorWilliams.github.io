c6d67b21a0454357568a756d66c39dd6b38bf589
Conor Williams <conorwilliams@outlook.com>
2023-01-28 10:37:23 +0000

    rm eWater

--- eWater/run.sh
+++
@@ -1,5 +0,0 @@
-for filename in *.csv; do
-    [ -e "$filename" ] || continue
-    echo 'Doing' $filename
-    taptimise $filename 10000 --scribble 200 --steps 10 --scales 1000
-done
--- generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'Village Reports' > index.html
-
-for filename in eWater/*.html; do
-    [ -e "$filename" ] || continue
-    echo '<p><a href="'$filename'">'$filename'</a></p>' >> index.html
-
-done
